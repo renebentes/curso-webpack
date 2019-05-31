fc06851acb4eafa2756cd410f3508afa53a9cd2e
Rene Bentes Pinto <renebentes@yahoo.com.br>
2019-05-31 10:22:24 -0300

    Aula 02 03 (#4)
    * Corrige caminho para dist
    
    * Adiciona loader
    
    * Atualiza README

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { abc } from './my-file';
+import jsLogo from './js-logo.png';
+
+const imgElement = document.querySelector('#my-image');
+imgElement.setAttribute('src', jsLogo);
 
 console.log('mensagem do index');
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,6 +4,14 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: { loader: 'url-loader' }
+      }
+    ]
   }
 };
