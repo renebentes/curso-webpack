09f71974b7a3eeb807d6e341773b0dc43bff3cd7
Rene Bentes Pinto <renebpinto@outlook.com>
2019-05-31 10:19:31 -0300

    Adiciona loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: { loader: 'url-loader' }
+      }
+    ]
   }
 };
