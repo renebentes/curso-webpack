96797376df2194ebf07bde19fb7e5d5aa7ca0198
Rene Bentes Pinto <rene_bentes@hotmail.com>
2019-08-27 21:08:09 -0300

    Adiciona jQuery

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import { abc } from './my-file';
 import jsLogo from './js-logo.png';
+import $ from 'jquery';
 
 const imgElement = document.querySelector('#my-image');
 imgElement.setAttribute('src', jsLogo);
+
+$(imgElement).on('click', () => {
+  $(this).hide();
+});
 
 console.log('mensagem do index');
 
